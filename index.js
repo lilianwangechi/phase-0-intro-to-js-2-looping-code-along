147971aceacb2563c11345f5a09c56bd2529ddfd
lilian <lilian.wangechi@student.moringaschool.com>
2022-08-12 04:40:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(arr,eventName){
+  
+  let arr1 = [];
+for(let i=0;i < arr.length;i++){
+  arr1.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+}
+return arr1;
+}
+
+function countDown(i){
+  
+  while(i >= 0 ){
+    console.log(i);
+    i--;
+  }
+}
